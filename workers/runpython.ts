95c55b43b0c638674a42249f002bc097770af443
Geoffrey Challen <geoffrey.challen@gmail.com>
2023-09-04 20:19:15 -0500

    Add support for formatting.

--- workers/runpython.ts
+++ workers/runpython.ts
@@ -5,8 +5,10 @@ let lineCount = 0
 const stdout: string[] = []
 
 let pyodide: PyodideInterface
+let loaded = false
 Promise.resolve().then(async () => {
   pyodide = await loadPyodide({ indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.4/full/" })
+
   pyodide.setStdout({
     batched: (line) => {
       lineCount++
@@ -17,16 +19,43 @@ Promise.resolve().then(async () => {
       }
     },
   })
-  pyodide.setInterruptBuffer
+
+  await pyodide.loadPackage("micropip")
+  const micropip = pyodide.pyimport("micropip")
+  await micropip.install("pycodestyle")
+  loaded = true
 })
 
 addEventListener(
   "message",
   async (event) => {
-    if (!pyodide) {
-      return RunPythonResponse.check({ error: "pyodide not loaded" })
+    if (!pyodide || !loaded) {
+      event.ports[0].postMessage(RunPythonResponse.check({ error: "pyodide not loaded" }))
+      return
     }
+
     const request = RunPythonRequest.check(event.data)
+
+    let code = request.code
+    if (!code.endsWith("\n\n")) {
+      code += "\n"
+    }
+
+    stdout.length = 0
+    lineCount = 0
+
+    const encodedCode = btoa(code)
+    const errorCount = await pyodide.runPythonAsync(`
+import pycodestyle
+from base64 import b64decode
+originalCode = b64decode("${encodedCode}").decode("utf-8")
+pep8style = pycodestyle.StyleGuide()
+count_errors = pep8style.input_file('stdin', lines=originalCode.splitlines(True))
+count_errors
+`)
+    if (errorCount > 0) {
+      event.ports[0].postMessage(RunPythonResponse.check({ error: stdout }))
+    }
 
     stdout.length = 0
     lineCount = 0
@@ -35,7 +64,7 @@ addEventListener(
     let error: unknown | undefined
 
     try {
-      await pyodide.runPythonAsync(request.code)
+      await pyodide.runPythonAsync(code)
       result = stdout.join("\n")
     } catch (_error) {
       error = _error
