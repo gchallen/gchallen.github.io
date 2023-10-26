e4b7759cae3b31f5bd7b91df62f0ca82d717644c
Geoffrey Challen <challen@illinois.edu>
2023-10-26 15:20:47 -0500

    Latest versions, Ace fixes.

--- components/Code.tsx
+++ components/Code.tsx
@@ -321,6 +321,8 @@ const Code: React.FC<{ codeId: string; originalCode: string; mode: string; meta:
         defaultValue={code}
         onBeforeLoad={(ace) => {
           ace.config.set("basePath", `https://cdn.jsdelivr.net/npm/ace-builds@${ace.version}/src-min-noconflict`)
+          ace.config.set("modePath", `https://cdn.jsdelivr.net/npm/ace-builds@${ace.version}/src-min-noconflict`)
+          ace.config.set("themePath", `https://cdn.jsdelivr.net/npm/ace-builds@${ace.version}/src-min-noconflict`)
         }}
         style={{ display: state === "loaded" ? "block" : "none" }}
         commands={commands}
