100cc5a921fee15655b456940e7ecb3236f63954
Geoffrey Challen <geoffrey.challen@gmail.com>
2024-11-24 14:09:41 -0600

    Latest.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const config = {
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+  },
   experimental: {
     scrollRestoration: true,
   },
